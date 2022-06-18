d53af195fc1f93704d78a83f57b79245b7ae70e3
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2022-06-18 13:35:37 -0300

    interface user repository

---
+++ src/main/java/br/com/andersondev/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.andersondev.api.repositories;
+
+import br.com.andersondev.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
