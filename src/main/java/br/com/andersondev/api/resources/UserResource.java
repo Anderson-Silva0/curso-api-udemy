b8fbd8e780c7bf9cc75215fe7dbada3885b32fb2
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2022-06-18 13:59:17 -0300

    classe user resource findById

--- src/main/java/br/com/andersondev/api/resources/UserResource.java
+++ src/main/java/br/com/andersondev/api/resources/UserResource.java
@@ -1,8 +1,11 @@
 package br.com.andersondev.api.resources;
 
 import br.com.andersondev.api.domain.User;
+import br.com.andersondev.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById() {
+    @Autowired
+    private UserService service;
 
-        return ResponseEntity.ok().body(new User (1,"anderson","ander@hotmail.com","1234"));
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
