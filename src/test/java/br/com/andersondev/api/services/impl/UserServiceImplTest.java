0256617baa9816dfe0103f3e70e2f566e134fd64
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2022-07-10 22:09:02 -0300

    teste para classe user service metodo findById objeto nao encontrado

--- src/test/java/br/com/andersondev/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/andersondev/api/services/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package br.com.andersondev.api.services.impl;
 import br.com.andersondev.api.domain.User;
 import br.com.andersondev.api.domain.dto.UserDTO;
 import br.com.andersondev.api.repositories.UserRepository;
+import br.com.andersondev.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -25,6 +26,7 @@ class UserServiceImplTest {
     public static final String NAME     = "Anderson";
     public static final String EMAIL    = "Anderson@gmail.com";
     public static final String PASSWORD = "123";
+    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
 
     @InjectMocks
     private UserServiceImpl service;
@@ -57,6 +59,18 @@ class UserServiceImplTest {
         assertEquals(ID, response.getId());
         assertEquals(NAME, response.getName());
         assertEquals(EMAIL, response.getEmail());
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnObjectNotFoundException(){
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
+
+        try{
+            service.findById(ID);
+        } catch (Exception ex){
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(OBJETO_NAO_ENCONTRADO, ex.getMessage());
+        }
     }
 
     @Test
